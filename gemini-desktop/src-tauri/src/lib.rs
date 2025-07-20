0fae9619cfdda274b8b8b098fa0b903687b3781b
Leandro Storoli <leandrostoroli@Mac.fritz.box>
2025-07-20 19:14:22 +0200

    Adjusting Desktop app

--- gemini-desktop/src-tauri/src/lib.rs
+++ gemini-desktop/src-tauri/src/lib.rs
@@ -6,9 +6,21 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    use tauri::Manager;
+    #[cfg(target_os = "macos")]
+    use window_vibrancy::{apply_vibrancy, NSVisualEffectMaterial};
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        // .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            let window = app.get_webview_window("main").unwrap();
+            #[cfg(target_os = "macos")]
+            {
+                apply_vibrancy(&window, NSVisualEffectMaterial::Titlebar, None, Some(0.8))
+                    .expect("Failed to apply vibrancy");
+            }
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
